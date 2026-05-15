df7644bde720c2f404e7a8d04052c9fc56fa55de
togle <togle@DESKTOP-CKVVBMQ>
2026-05-15 15:47:17 +0300

    Added Shopdesk update functionality; Added Windows activation functionality; Implemented the locking of function keys during operations; Added program activity logging.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace b2b_support_tool
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
